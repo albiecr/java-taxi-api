1139299490d645af57091f0c7fe26098bedadb58
albiecr <albertinacrodrigues@gmail.com>
2025-11-08 22:41:14 -0300

    feat: create JPA Repository for Passenger

--- src/main/java/com/taxiapp/taxi_api/repository/PassengerRepository.java
+++ src/main/java/com/taxiapp/taxi_api/repository/PassengerRepository.java
@@ -10,8 +10,8 @@ import com.taxiapp.taxi_api.model.Passenger;
 
 
 @Repository
-public interface PassengerRepository extends JpaRepository<Passenger, Long> {
-    Optional<Passenger> findByName(String name);
+public interface PassengerRepository extends JpaRepository<Passenger, Long> {       // JpaRepository already comes with all the CRUD methods ready.
+    Optional<Passenger> findByName(String name);                                    // Custom query methods based on field names
     Optional<Passenger> findByPhone(String phone);
     Optional<Passenger> findByEmail(String email);
     Optional<Passenger> findByUsername(String username);
